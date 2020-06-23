10409d5bf1553cf7d5147b20ae384a10b45b8dc7
Denis Fitzpatrick <denisvanfitzpatrick@gmail.com>
2020-06-23 08:50:14 -0500

    Added OSX executable

--- application.macosx/source/SimonTask.java
+++ application.macosx/source/SimonTask.java
@@ -16,26 +16,30 @@ import java.io.IOException;
 
 public class SimonTask extends PApplet {
 
-
-Button [] buttons = new Button[4];
-
+Button [] buttons = new Button[4]; //<>// //<>//
+int playerToneTime = 100;
 DoneButton doneButton;
-
+int textsize = 64;
 SimonToneGenerator simonTones;
 float myAmp=0.0f;
 int [] colorsstart = {0xff00ff00, 0xffff0000, 0xffffff00, 0xff0000ff}, colors = new int[4]; 
 int bgcolor = 255; //black = 0, 128 gray, 255 white; 
 IntList colorscrambler = new IntList();
 int index;
-
+int instructNum = 1;
 int [] simonSentence = new int[32];
 int [] simonSentenceSave = new int[32];
 int positionInSentence = 0;
-int currentLengthOfTheSentence = 0;
+int currentLengthOfTheSentence = 2;
 int wrongCount = 0;
 int clickCount = 0;
+
+int lengthPresented ;
+int practiceCount = 0;
+
 int talkTime = 700;
-int playerToneTime = 100;
+int sequence = 0;
+
 int hideTime = 700;
 int nextTurnTime = 0;
 int timeOut = 0;
@@ -43,27 +47,42 @@ int circSize = 300;
 boolean isRandom;
 int runNum = 0;
 int trialCount = 0;
-int maxRun = 4;
+int maxRun = 30;
 String[] colorAssign = {"", "", "", ""};
 
-boolean isSimonsTurn = true;
-boolean lastClick = true;
-int [] runtype = new int[4];
-int [] runlength = new int[4];
+String [] circleInstructions;
+String [] doPractice;
+String [] goAhead;
+String circleInstructionsText, doPracticeText, goAheadText;
+
+boolean isSimonsTurn = true, instruct = true, notPractice = false;
+boolean lastClick = true, simonStart=true;
+int [] runtype = new int[maxRun];
+int [] runlength = new int[maxRun];
 Table table;
 int rowCount = 0;
 public void setup() {
   
   background(bgcolor);
+  textSize(textsize);
+  textAlign(CENTER);
   String[] lines = loadStrings("parameters.txt");
+  circleInstructions = loadStrings("CircleInstructions.txt");
+  doPractice = loadStrings("DoPractice.txt");
+  goAhead = loadStrings("GoAhead.txt");
+  circleInstructionsText = join(circleInstructions, "\n");
+  doPracticeText = join(doPractice, "\n");
+  goAheadText = join(goAhead, "\n");
+
   String[] list;
   for (int i = 0; i < lines.length; i++) {
     list = split(lines[i], " ");
     runtype[i] = PApplet.parseInt(list[0]);
     runlength[i] = PApplet.parseInt(list[1]);
-    println(str(runtype[i])+" "+str(runlength[i]));
+    //println(str(runtype[i])+" "+str(runlength[i]));
   }
   maxRun = lines.length;
+  println(maxRun);
   textSize(40);
   isRandom = runtype[runNum]==1;
   strokeWeight(4);
@@ -74,7 +93,7 @@ public void setup() {
     colors[i] = colorsstart[index];
     colorAssign[i] = hex(colors[i], 6);
   }
-  saveStrings("colors.txt", colorAssign);
+
   doneButton = new DoneButton(1, width*6/8, height*6/8, width/8, height/8);
   buttons[0] = new Button(0, width/2, height/4, circSize, colors[0]);
   buttons[1] = new Button(1, width/4, height/2, circSize, colors[1]);
@@ -82,11 +101,15 @@ public void setup() {
   buttons[3] = new Button(3, width/2, height*3/4, circSize, colors[3]);
 
   table = new Table();
-  table.addColumn("runNum");
+  table.addColumn("Block");
+  table.addColumn("RowCount");
   table.addColumn("position");
+  table.addColumn("lengthPresented");
   table.addColumn("circleNum");
+  table.addColumn("correct");
   table.addColumn("source");
   table.addColumn("isRandom");
+  table.addColumn("TrialCount");
   table.addColumn("time");
 
   textAlign(CENTER, CENTER);
@@ -96,57 +119,75 @@ public void setup() {
 
   textSize(40);
   textAlign(CENTER, CENTER);
-  
-  makeNewSentence();
-  arrayCopy(simonSentence, simonSentenceSave);
-    println(join(nf(simonSentenceSave, 0), ", "));
 
-  simonStartsNewGame(isRandom);
+  makeNewSentence(0); // these two statments make the fixed sequence
+  arrayCopy(simonSentence, simonSentenceSave);
+  //println(join(nf(simonSentenceSave, 0), ", "));
+  makeNewSentence(1);
+
+  //simonStartsNewGame(isRandom);
 }
 
 public void draw() {
   background(bgcolor);
-  simonTones.checkPlayTime();
-
-  if (simonTones.isPlayingTone == false) setButtonLightsOff();
-
-  if (isSimonsTurn) simonSays();
-  int buttonCount = 0;
-  for (Button currentButton : buttons) {
-    if (isSimonsTurn) {
-      doneButton.isDisplayed = false;
-      currentButton.myDarkColor = color(255);
-      currentButton.myColor = currentButton.myDefaultColor;
-    } else {
-      if (millis() > nextTurnTime) {
-        doneButton.isDisplayed = true;
-        currentButton.myDarkColor = currentButton.myDefaultColor;
-        currentButton.myColor = color(0);
-        buttonCount++;
-        if (buttonCount> 3) {
-          nextTurnTime = millis()+100000;
+  if (instruct) {
+    textSize(textsize/2);
+    fill(0);
+    if (instructNum ==1)  text(circleInstructionsText, width/16, height/8, width*7/8, height*3/4);
+    if (instructNum ==2)  text(doPracticeText, width/16, height/8, width*7/8, height*3/4);
+    if (instructNum ==3)  text(goAheadText, width/16, height/8, width*7/8, height*3/4);
+    textSize(textsize);
+  } else {
+    simonTones.checkPlayTime();
+    if (simonTones.isPlayingTone == false) setButtonLightsOff();
+    if (isSimonsTurn) simonSays();
+    int buttonCount = 0;
+    for (Button currentButton : buttons) {
+      if (isSimonsTurn) {
+        doneButton.isDisplayed = false;
+        currentButton.myDarkColor = color(255);
+        currentButton.myColor = currentButton.myDefaultColor;
+      } else {
+        if (millis() > nextTurnTime) {
+          doneButton.isDisplayed = true;
+          currentButton.myDarkColor = currentButton.myDefaultColor;
+          currentButton.myColor = color(0);
+          buttonCount++;
+          if (buttonCount> 3) {
+            nextTurnTime = millis()+100000;
+          }
         }
       }
+      currentButton.display();
     }
-    currentButton.display();
+    doneButton.display();
+    fill(255);
   }
-  doneButton.display();
-  fill(255);
 }
 
 public void simonSays() {
 
   if (millis() >= timeOut) {
-
     int simonsWord = simonSentence[positionInSentence];
-    table.setFloat(rowCount, "time", millis());
-    table.setInt(rowCount, "runNum", runNum);
-    table.setInt(rowCount, "position", positionInSentence);
-    table.setInt(rowCount, "circleNum", simonsWord);
-    table.setInt(rowCount, "source", 1);  //1 = Simon
-    table.setInt(rowCount, "isRandom", runtype[runNum]);
-    rowCount++;
-    println(str(simonsWord));
+    if (notPractice) {
+      if (simonStart) {
+        sequence++;
+        simonStart = false;
+      }
+      lengthPresented = positionInSentence + 1;
+      table.setFloat(rowCount, "time", millis());
+      table.setInt(rowCount, "Block", runNum - 1);
+      table.setInt(rowCount, "RowCount", rowCount + 1);
+      table.setInt(rowCount, "position", positionInSentence + 1);
+      table.setInt(rowCount, "lengthPresented", lengthPresented);
+      table.setInt(rowCount, "TrialCount", sequence);
+      table.setInt(rowCount, "circleNum", simonsWord + 1);
+      table.setInt(rowCount, "correct", 1);  //1 = Simon
+      table.setInt(rowCount, "source", 1);  //1 = Simon
+      table.setInt(rowCount, "isRandom", runtype[runNum]);
+      rowCount++;
+    }
+    //println(str(simonsWord));
     simonTones.playTone(simonsWord, talkTime, myAmp);
     buttons[simonsWord].isLightOn = true;
     if (positionInSentence < currentLengthOfTheSentence) {
@@ -165,24 +206,43 @@ public void simonSays() {
 public void mousePressed() {
 
   if (doneButton.isMouseOver() == true) {
+    if (notPractice) {
+      simonStart = true;
+    } else {
+      if ((wrongCount == 0) && (clickCount - 1 == currentLengthOfTheSentence)) {
+        instruct = true;
+        instructNum++;
+        if (instructNum == 2) {
+          makeNewSentence(2);
+        }
+      }
+    }
     doneButton.isDisplayed = false;
     isSimonsTurn = true;
     timeOut = millis() + talkTime + hideTime;
   } else { //a target button is pressed
     if (isSimonsTurn == false) { //only check clicks during player's turn
-      clickCount++;
       for (Button currentButton : buttons) {
         if (currentButton.isMouseOver() == true) {
+          clickCount++;
           currentButton.isLightOn = true;
-          table.setFloat(rowCount, "time", millis());
-          table.setInt(rowCount, "runNum", runNum);
-          table.setInt(rowCount, "position", positionInSentence);
-          table.setInt(rowCount, "circleNum", currentButton.myId);
-          table.setInt(rowCount, "source", 0);  //0 = player
-          table.setInt(rowCount, "isRandom", runtype[runNum]);
-          rowCount++;
-
-          println(str(currentButton.myId));
+          if (notPractice) {
+            table.setFloat(rowCount, "time", millis());
+            table.setInt(rowCount, "Block", runNum - 1);
+            table.setInt(rowCount, "RowCount", rowCount + 1);
+            table.setInt(rowCount, "position", positionInSentence + 1);
+            table.setInt(rowCount, "lengthPresented", lengthPresented);
+            table.setInt(rowCount, "TrialCount", sequence);
+            table.setInt(rowCount, "circleNum", currentButton.myId + 1);
+            if ((simonSentence[positionInSentence] == currentButton.myId) && (positionInSentence <= lengthPresented))  {
+              table.setInt(rowCount, "correct", 1);
+            } else {
+              table.setInt(rowCount, "correct", 0);
+            }
+            table.setInt(rowCount, "source", 0);  //0 = player
+            table.setInt(rowCount, "isRandom", runtype[runNum]);
+            rowCount++;
+          }
           if (simonSentence[positionInSentence] != currentButton.myId) {//wrong
             simonTones.playTone(4, playerToneTime, myAmp);
             wrongCount++;
@@ -199,15 +259,21 @@ public void mouseReleased() {
   //println("released!");
   if (doneButton.isMouseOver() == true) {
     if ((wrongCount == 0) && (clickCount - 1 == currentLengthOfTheSentence)) {
-      currentLengthOfTheSentence++;
+      if (notPractice) {
+        currentLengthOfTheSentence++;
+      }
     } else {
-      currentLengthOfTheSentence--;
+      if (notPractice) {
+        currentLengthOfTheSentence--;
+      }
       if (currentLengthOfTheSentence < 0)
         currentLengthOfTheSentence = 0;
     }
     trialCount++;
     if (trialCount==runlength[runNum]) {
       runNum++;
+      if (runNum == 2) notPractice = true;
+      println(runNum);
       if (runNum==maxRun) {
         exit();
       }
@@ -215,11 +281,11 @@ public void mouseReleased() {
       trialCount = 0;
     }
     if (isRandom) {
-      makeNewSentence();
+      makeNewSentence(0);
     } else {
-      arrayCopy(simonSentenceSave, simonSentence);
+      if (notPractice) arrayCopy(simonSentenceSave, simonSentence);
     }
-    wrongCount = 0;//each trun starts you over
+    wrongCount = 0;//each turn starts you over
     clickCount = 0;
     lastClick = true;
     println("LastClick");
@@ -228,7 +294,6 @@ public void mouseReleased() {
     timeOut = millis() + talkTime + hideTime;
   } else {
     if (isSimonsTurn == false) {
-
       if (positionInSentence < currentLengthOfTheSentence) {
         positionInSentence++;
       } else {  //positionInSentence >= currentLengthOfTheSentence
@@ -251,7 +316,7 @@ public void setButtonLightsOff() {
 
 public void simonStartsNewGame(boolean isRandom) {
   if (isRandom) {
-    makeNewSentence();
+    makeNewSentence(0);
   } else {
     arrayCopy(simonSentenceSave, simonSentence);
   }
@@ -260,13 +325,39 @@ public void simonStartsNewGame(boolean isRandom) {
   isSimonsTurn = true;
 }
 
-public void makeNewSentence() {
+public void makeNewSentence(int predefined) {
   for (int i = 0; i<simonSentence.length; i++) {
     simonSentence[i] = PApplet.parseInt(random(0, 4));
   }
+  if (predefined==1) {
+    simonSentence[0] = 0;
+    simonSentence[1] = 2;
+    simonSentence[2] = 2;
+  };
+  if (predefined==2) {
+    simonSentence[0] = 1;
+    simonSentence[1] = 3;
+    simonSentence[2] = 1;
+  }
+
 
   positionInSentence = 0;
-  println(join(nf(simonSentence, 0), ", "));
+  //println(join(nf(simonSentence, 0), ", "));
+}
+
+public void keyPressed() {
+  if (key == ' ' ) {
+    instruct = false;
+    if (instructNum == 3) {
+      instruct = false;
+      notPractice = true;
+      currentLengthOfTheSentence = 0;
+      setButtonLightsOff();
+      doneButton.isDisplayed = false;
+      isSimonsTurn = true;
+      timeOut = millis() + talkTime + hideTime;
+    }
+  }
 }
 
 public void exit() {
